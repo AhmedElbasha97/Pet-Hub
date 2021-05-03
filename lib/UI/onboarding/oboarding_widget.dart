2ec3a66fca071c8cd94177be148705fff57c7559
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-03 23:19:00 +0200

    adding variabls

---
+++ lib/UI/onboarding/oboarding_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OnboeardWidget extends StatelessWidget {
+  final String assetlink;
+  final String title;
+  final String subTitile;
+  final bool isactive;
+
+  OnboeardWidget({
+    this.assetlink,
+    this.isactive,
+    this.title,
+    this.subTitile,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
