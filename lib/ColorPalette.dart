4fde1dd5cc7edd4873283cbb2d65a63d098d1161
Mohamed Osama Hassan <58494953+Eng-Mohamed-Osama@users.noreply.github.com>
2021-05-04 05:09:41 +0200

    adding colorPalette  class

---
+++ lib/ColorPalette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  const ColorPalette();
+
+  static const Color mainColor = const Color(0xff16bbc7);
+  static const Color secondaryColor = const Color(0xff0d3f67);
+}
